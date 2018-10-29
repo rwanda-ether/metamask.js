bdb264980485e637a6c92adf57ca7a2233b3953b
rwanda-ether <rwanda-ether@user.noreplay.github.com>
2018-10-30 00:29:59 +0900

    Everything is practice. -- Pele

--- sample.001/js/web3.js
+++ sample.001/js/web3.js
@@ -1,4 +1,11 @@
-//Time-stamp: <2018-10-29 21:46:54 hamada>
+//Time-stamp: <2018-10-30 00:28:02 hamada>
+
+if( ('sessionStorage' in window) && (window.sessionStorage !== null) ) {
+    console.log("SessionStorage: Available!");
+} else {
+    console.log("SessionStorage: Not available...");
+}
+
 
 window.addEventListener('load', () => {
 		if (typeof web3 !== 'undefined') {
@@ -10,30 +17,47 @@ window.addEventListener('load', () => {
 		}
 });
 
+
 function onlyRopstenTestNetwork(callback) {
   web3.version.getNetwork((err, netId) => {
     if (netId === "3") {
-      callback();
+				//callback();
+				setInterval(callback, 300);
     } else {
-      document.write("Please switch MetaMask to Ropsten Test Network and reload page.");
+				document.write("Please switch MetaMask to Ropsten Test Network and reload page.");
     }
   });
 }
 
-function main() {
-		var user = {
-				account: null,
-				balance: null
-		}
-		user.account = web3.eth.defaultAccount;
-		user.balance = 0;
-		web3.eth.getBalance(user.account, (err, balance) => {
+function saveAccountInfo2storage(_account) {
+		web3.eth.getBalance(_account, (err, _wei) => {
 				if (err) return;
-				console.log(JSON.stringify(balance, undefined, 1));
-				var text02 = document.getElementById('text02');
-				var v  = web3.fromWei(balance, "ether");
-				text02.textContent = 'balance: '+v;
+				var v = web3.fromWei(_wei, "ether");
+				sessionStorage.clear();
+				sessionStorage.metamask_account = _account;
+				sessionStorage.metamask_balance = v;
+				console.log('v: '+v);
 		});
+}
+
+function main() {
+		var account = web3.eth.defaultAccount;
+		saveAccountInfo2storage(account);
+		var balance = sessionStorage.getItem('metamask_balance');
+		balance = Number(balance);
+		console.log(balance);
+
 		var text01 = document.getElementById('text01');
-		text01.textContent = 'account: '+user.account;
+		var text02 = document.getElementById('text02');
+		var text03 = document.getElementById('text03');
+
+		if(balance > 31.0){
+				text01.textContent = 'Success!!';
+				text02.textContent = 'account: '+account;
+				text03.textContent = balance + " ETH";
+		}else{
+				text01.textContent = 'Failed...';
+				text02.textContent = 'account: '+account;
+				text03.textContent = "you only have "+balance+" ETH! not enough !!"
+		}
 }
